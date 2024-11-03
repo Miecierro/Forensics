e0cdf8efbe9f043bb415711bb85f3efd14daa6e5
Magdalena-Kochanska <46958943+Magdalena-Kochanska@users.noreply.github.com>
2024-11-03 16:04:36 +0100

    Update forensics.py
    dodanie funkcji do analizy plików bazując na obrazie

--- forensics.py
+++ forensics.py
@@ -3,14 +3,23 @@ import subprocess
 import hashlib
 import time
 from datetime import datetime
+import re
 
 def log_message(message):
     """Zapisz wiadomość do pliku logu"""
     with open("system_log.txt", "a") as log_file:
         log_file.write(f"{datetime.now()} - {message}\n")
     print(message)
+    
+def raport_message(message):
+    """Zapisz wiadomość do pliku raportu"""
+    with open("raport_mess.txt", "a") as log_file:
+        log_file.write(f"{message}\n")
+    print(message)
+
 
 def document_vm_configuration():
+    
     """Zapisuje informacje o konfiguracji maszyny wirtualnej do pliku"""
     try:
         # Pobierz podstawowe informacje o systemie
@@ -49,8 +58,6 @@ def detect_and_mount_drive():
         os.makedirs(mount_point, exist_ok=True)
         subprocess.run(f"sudo mount -o ro /dev/{drive} {mount_point}", shell=True, check=True)
         log_message(f"Nośnik /dev/{drive} został zamontowany w {mount_point}.")
-
-        return mount_point
     except Exception as e:
         log_message(f"Błąd podczas wykrywania i montowania nośnika: {e}")
         return None
@@ -74,10 +81,54 @@ def create_checksum_and_image(mount_point):
                 sha256_hash.update(byte_block)
         with open(checksum_path, "w") as f:
             f.write(sha256_hash.hexdigest())
-
+        
         log_message(f"Suma kontrolna zapisana w pliku {checksum_path}.")
     except Exception as e:
         log_message(f"Błąd podczas tworzenia obrazu lub sumy kontrolnej: {e}")
+
+def check_partition(mount_point): #gdzie mount_point /dev/NAME
+    """Analizuje strukturę partycji za pomocą fdisk i mmls."""
+    
+    # Sprawdzanie struktury partycji za pomocą `fdisk`
+    fdisk_output = subprocess.run(["sudo","fdisk", "-l", mount_point], capture_output=True, text=True)
+    if fdisk_output.returncode == 0:
+        for line in fdisk_output.stdout.splitlines():
+            if "Units:" in line:  # Filtrujemy tylko linie dotyczące partycji
+                match = re.search(r'(\d+)$', line)
+                if match:
+                    offset = match.group(1)  # Pobieramy znalezioną liczbę
+        log_message(f"\nStruktura partycji przy użyciu fdisk zapisana do pliku raportu.")
+        raport_message(f"\nStruktura partycji przy użyciu fdisk:")
+        raport_message(fdisk_output.stdout)
+        
+    else:
+        print(f"Błąd fdisk: {fdisk_output.stderr}")
+        log_message(f"Błąd fdisk: {fdisk_output.stderr}")
+    return offset
+
+def file_analysis(mount_point):
+    """Analizuje metadane plików za pomocą fls i istat."""
+    # Użycie `fls` do wylistowania plików na pierwszej partycji
+    try:
+        fls_output = subprocess.run(["sudo","fls", mount_point])  #gdzie mount_point /dev/NAME
+        
+        if fls_output.returncode == 0:
+            raport_message(f"\nWylistowanie plików przy użyciu fls:")
+            raport_message(fls_output.stdout)
+            log_message(f"\nWylistowanie plików przy użyciu fls zapisana do pliku raportu.")
+            
+            inode_ids = re.findall(r"\* (\d+):", fls_output)  # Pobranie numerów inode
+            for inode in inode_ids:
+                print(f"\nMetadane pliku o inode {inode}:")
+                istat_output = subprocess.run(["sudo","istat", mount_point, inode])
+                print(istat_output)
+        else:
+            print(f"Błąd fdisk: {fls_output.stderr}")
+            log_message(f"Błąd fdisk: {fls_output.stderr}")
+    except Exception as e:
+        log_message(f"Błąd podczas wykrywania i montowania nośnika: {e}")
+        return None
+
 
 def main():
     """Główna funkcja kontrolująca działanie skryptu"""
@@ -92,6 +143,12 @@ def main():
         if mount_point:
             # Krok 3: Tworzenie obrazu i sumy kontrolnej
             create_checksum_and_image(mount_point)
+            
+            # Krok 4: Badanie struktury partycji obrazu 
+            check_partition(mount_point)
+            
+            # Ktok 5: Analiza plików
+            file_analysis(mount_point)
 
             # Odmontowanie nośnika po zakończeniu pracy
             subprocess.run(f"sudo umount {mount_point}", shell=True)
