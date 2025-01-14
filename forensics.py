47c096387bab1b931a2de8f510f43ad1baa1ebf2
Miecierro <259390@student.pwr.edu.pl>
2025-01-14 19:21:38 +0100

    Add files via upload

--- forensics.py
+++ forensics.py
@@ -10,22 +10,22 @@ def log_message(message):
     with open("system_log.txt", "a") as log_file:
         log_file.write(f"{datetime.now()} - {message}\n")
     print(message)
-    
+
 def raport_message(message):
     """Zapisz wiadomość do pliku raportu"""
     with open("raport_mess.txt", "a") as log_file:
         log_file.write(f"{message}\n")
     print(message)
 
-def hash_file(ext_drive):
+def hash_file_original(ext_drive):
     """Obliczenie hasha"""
     output_file = f"{ext_drive}_sha256.txt"
     file_path = f"/dev/{ext_drive}"
 
     try:
         # Wywoluje komende ktora tworzy hash i zapisuje do pliku
-        with open(output_file, "w") as f:
-            subprocess.run(["sudo", "sha256sum", file_path], stdout=f, check=True)
+        with open(output_file, "a") as f:
+            subprocess.run(["sudo", "sha1sum", file_path], stdout=f, check=True)
         log_message(f" Generowanie sumy kontrolnej przebieglo pomyslnie")
 
     # Błąd podczas uruchamiania polecenia subprocess
@@ -34,8 +34,27 @@ def hash_file(ext_drive):
 
     # Błąd podczas pracy z plikiem
     except IOError as e:
-        log_message(f"Błąd podczas zapisu do pliku: {e}") 
-         
+        log_message(f"Błąd podczas zapisu do pliku: {e}")
+
+def hash_file_copy(ext_drive):
+    """Obliczenie hasha"""
+    output_file = f"{ext_drive}_backup_sha256.txt"
+    file_path = f"{ext_drive}_backup.img"
+
+    try:
+    # Wywoluje komende ktora tworzy hash i zapisuje do pliku
+        with open(output_file, "a") as f:
+            subprocess.run(["sudo", "sha1sum", file_path ], stdout=f, check=True)
+        log_message(f" Generowanie sumy kontrolnej przebieglo pomyslnie")
+
+    # Błąd podczas uruchamiania polecenia subprocess
+    except subprocess.CalledProcessError as e:
+        log_message("Błąd podczas generowani sumy kontrolnej: {e}")
+
+    # Błąd podczas pracy z plikiem
+    except IOError as e:
+        log_message(f"Błąd podczas zapisu do pliku: {e}")
+
 def document_vm_configuration():
     """Zapisuje informacje o konfiguracji maszyny wirtualnej do pliku"""
     try:
@@ -71,9 +90,9 @@ def detect_name_drive():
     except Exception as e:
         log_message(f"Błąd podczas szukania urzadzenia: {e}")
         return None
-      
+
 def detect_drive():
-    """Automatyczne wykrywanie nowych nośników i montowanie ich w trybie read-only"""   
+    """Automatyczne wykrywanie nowych nośników i montowanie ich w trybie read-only"""
     # Wykrywanie podłączonych urządzeń
     try:
             initial_drives = subprocess.run(["lsblk", "-r", "-o", "NAME"], capture_output=True, text=True, check=True).stdout.strip().split("\n")
@@ -88,9 +107,9 @@ def detect_drive():
                     log_message(f"Nowe urządzenie zostało podłączone: {new_device[0]}")
                     return new_device[0]
                 time.sleep(2)
-            log_message("Błąd: Nie wykryto nowego urządzenia w czasie oczekiwania.")
+            log_message(f"Nie wykryto nowego urządzenia w czasie oczekiwania.")
             return None
-    
+
     except subprocess.SubprocessError as e:
             log_message(f"Błąd podczas wykrywania urządzeń: {e}")
             return None
@@ -116,11 +135,14 @@ def detect_drive():
 #             print("Nowe urządzenie zostało podłączone:", new_device)
 #             return new_device
 #        # Zakładamy, że pierwszy znaleziony napęd jest tym, który należy zamontować
-               
+
 def create_checksum_and_image(ext_drive):
     """Tworzy sumę kontrolną oraz kopię obrazu nośnika"""
     try:
         image_path = f"{ext_drive.replace('/mnt/', '')}_backup.img"
+        """Obliczenie hasha przed kopiowaniem"""
+        hash_file_original(ext_drive)
+        log_message(f" Generowanie sumy kontrolnej przed kopiowaniem obrazu")
 
         # Tworzenie obrazu za pomocą dd
         log_message(f"Rozpoczęto tworzenie obrazu {image_path}...")
@@ -129,7 +151,9 @@ def create_checksum_and_image(ext_drive):
         log_message(f"Obraz nośnika zapisany jako {image_path}.")
 
         # Tworzenie sumy kontrolnej oryginalnego nośnika
-        hash_file(ext_drive)
+        """Obliczenie hasha po kopiowaniu"""
+        hash_file_original(ext_drive)
+        log_message(f" Generowanie sumy kontrolnej po kopiowaniu obrazu")
 
         ## Odmontywanie oryginalnego nośnika
         #subprocess.run(['sudo','umount',f"/dev/{ext_drive}"], check=True)
@@ -137,13 +161,16 @@ def create_checksum_and_image(ext_drive):
 
     except Exception as e:
         log_message(f"Błąd podczas tworzenia obrazu: {e}")
-        
-        
+
+
 def mount_from_img(ext_drive):
     # Montowanie nowego nośnika dysku
-    try: 
+    try:
         image_path = f"{ext_drive.replace('/mnt/', '')}_backup.img"
         log_message(f"Rozpoczęto montowanie obrazu {image_path}...")
+        """Obliczenie hasha przed zamontowaniem"""
+        hash_file_copy(ext_drive)
+        log_message(f" Generowanie sumy kontrolnej przed zamontowaniem obrazu")
 
         # Tworzenie katalogu dla kopii zapasowej
         if not os.path.exists(f"/media/{ext_drive}_backup"):
@@ -158,12 +185,13 @@ def mount_from_img(ext_drive):
         log_message(f"Obraz kopii nośnika poprawnie zamontowany.")
 
         # Generowanie sumy kontrolnej dla nowego nośnika po zamontowaniu
-        hash_file(detect_name_drive())
+        """Obliczenie hasha po zamontowaniu"""
+        hash_file_copy(ext_drive)
+        log_message(f" Generowanie sumy kontrolnej po zamontowaniu obrazu")
 
-        #STWORZENIE FUNKCJI ŻEBY PORÓWNYWAŁA CHECKSUM STAREGO I NOWEGO 
 
     except Exception as e:
-        log_message(f"Błąd podczas montowania obrazu {e}") 
+        log_message(f"Błąd podczas montowania obrazu {e}")
 
 def check_partition(ext_drive): #gdzie mount_point /dev/NAME
     """Analizuje strukturę partycji za pomocą fdisk i mmls."""
@@ -183,7 +211,7 @@ def check_partition(ext_drive): #gdzie mount_point /dev/NAME
                     #    offset = match.group(1)  # Pobieramy znalezioną liczbę ### NIGDZIE NIE JEST UŻYTA ZMIENNA PO CO ONA
                         log_message(f"\nStruktura partycji przy użyciu fdisk zapisana do pliku raportu.")
                         raport_message(f"\nStruktura partycji przy użyciu fdisk {fdisk_output.stdout}")
-            
+
     except subprocess.CalledProcessError as e:
         log_message(f"Błąd podczas analizy partycji za pomocą fdisk: {e}")
         raport_message(f"Błąd fdisk:\n{e.stderr}")
@@ -196,26 +224,30 @@ def file_analysis(ext_drive):
     log_message(f"Analiza metadanych plików na urządzeniu {ext_drive}.")
 
     path_to_mount = "/dev/"+ ext_drive
-    
+
     try:
-        fls_output = subprocess.run(["sudo","fls", path_to_mount], capture_output=True, text=True, check=True) 
-        
+        fls_output = subprocess.run(["sudo","fls", path_to_mount], capture_output=True, text=True, check=True)
+
         raport_message(f"\nWylistowanie plików przy użyciu fls:")
         raport_message(fls_output.stdout)
-        log_message(f"\nWylistowanie plików przy użyciu fls zapisana do pliku raportu.")	
-        
+        log_message(f"\nWylistowanie plików przy użyciu fls zapisana do pliku raportu.")
+
         #Pobieranie numerów inode
         inode_ids = re.findall(r'\b\d{2,6}\b', fls_output.stdout)
         if not inode_ids:
             log_message("Nie znaleziono żadnych numerów inode w wynikach fls")
             raport_message("Nie znaleziono żadnych numerów inode w wynikach fls")
-        
+
         for inode in inode_ids:
             log_message(f"\nAnaliza metadanych o inode {inode}:")
             istat_output = subprocess.run(["sudo","istat", path_to_mount, inode], capture_output=True, text=True, check=True)
             raport_message(f"\n=== Metadane pliku o inode {inode} ===\n")
             raport_message(istat_output.stdout)
             log_message(f"Metadane pliku o inode {inode} zapisano do raportu")
+
+        """Obliczenie hasha po analizie obrazu"""
+        hash_file_copy(ext_drive)
+        log_message(f" Generowanie sumy kontrolnej po analizie obrazu")
         return 1
 
     except subprocess.CalledProcessError as e:
@@ -225,14 +257,15 @@ def file_analysis(ext_drive):
     except Exception as e:
         log_message(f"Nieoczekiwany błąd podczas analizy plików: {e}")
         return 0
-        
+
+
 def main():
     """Główna funkcja kontrolująca działanie skryptu"""
     log_message("=== Rozpoczęcie pracy skryptu ===")
-    
+
     # Krok 1: Dokumentacja konfiguracji VM
     document_vm_configuration()
-    
+
     while True:
         # Krok 2: Wykrywanie i montowanie nowego nośnika
         ext_drive = detect_drive()
@@ -241,32 +274,27 @@ def main():
         if ext_drive:
             # Krok 3: Tworzenie kopii oraz sumy kontrolnej oryginalnego obrazu i odmontowanie go
             create_checksum_and_image(ext_drive)
-            
-            # Krok 3: Zamontowanie nowego nosnika z obrazu 
+
+            # Krok 3: Zamontowanie nowego nosnika z obrazu
             mount_from_img(ext_drive)
-            
+
             # Zapisanie nazwy ścieżki do zmiennej
             # Z TEGO CO ROZUMIEM TO ZAPISUJE ŚĆIEŻKĘ DO TEGO ORYGINALNEGO NOŚNIKA | W TYM MOMENCIE JUŻ NIE POWINNIŚMY NA NIM PRACOWAĆ | W SENSIE NIE MA TO AŻŻŻŻŻ TAKIEJ RÓŻNICY, BO JEST TO W READ-ONLY, ALE TO NIE JEST DOBRA PRAKTYKA
             new_drive = detect_name_drive()
-            
-            # Krok 4: Badanie struktury partycji obrazu 
+
+            # Krok 4: Badanie struktury partycji obrazu
             check_partition(new_drive)
-            
+
             # Ktok 5: Analiza plików
             if file_analysis(new_drive) == 1:
                 break
 
-            # Odmontowanie nośnika po zakończeniu pracy SPRAWDZIĆ CZYM TO SIĘ RÓŻNI OD LINIJEK 134-136
+            # Odmontowanie nośnika po zakończeniu pracy 
             subprocess.run(f"sudo umount {ext_drive}", shell=True)
             log_message(f"Nośnik {ext_drive} został odmontowany.")
-        
+
         # Odczekaj przed kolejną iteracją (np. 60 sekund)
         time.sleep(60)
-################################### NIE SPRAWDZIŁEM JESZCZE TEGO CZY SUMY KONTROLNE SA ZAPISYWANE PO UKONCZENIU PROCESU ZAUTOMATYZOWANEJ ANALIZY, POWINNY BYĆ W SUMIE CZTERY SUMY KONTROLNE, DWIE OBRAZU ORYGINALU, DWIE KOPII
-################################### JEDNA (ORYGINALU) PRZED ZAMONTOWANIEM
-################################### DRUGA (ORYGINAŁu) PO STWORZENIU KOPII 
-################################### TRZECIA (KOPII) PRZED ODMONTOWANIEJ ORYGINAŁU
-################################### CZWARTA (KOPII) PO ZAKOŃCZENIU ANALIZY
 
 if __name__ == "__main__":
     main()
